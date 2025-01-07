373663db082c72e1dff5a5c369921ce3fa43d051
carlosportella16 <carlosportella16@hotmail.com>
2025-01-07 17:15:01 -0300

    Revert "Fix: VPC and subnets"
    This reverts commit 558ba2f7e7ca32fe0d38da84aadae82bdccd44b7.

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,39 +1,14 @@
 variable "vpc_cidr_block" {
-  type        = string
-  description = "CIDR principal da VPC."
-  default     = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
-# Mapeia cada AZ para um bloco CIDR de subnet pública
 variable "public_subnets_cidrs" {
-  type = map(string)
-  description = <<EOT
-Mapeamento de AZ para CIDR de subnet pública.
-Exemplo:
-{
-  "us-east-1a" = "10.0.0.0/24",
-  "us-east-1b" = "10.0.1.0/24"
-}
-EOT
-  default = {
-    "us-east-1a" = "10.0.0.0/24"
-    "us-east-1b" = "10.0.1.0/24"
-  }
+  type    = list(string)
+  default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
-# Mapeia cada AZ para um bloco CIDR de subnet privada
 variable "private_subnets_cidrs" {
-  type = map(string)
-  description = <<EOT
-Mapeamento de AZ para CIDR de subnet privada.
-Exemplo:
-{
-  "us-east-1a" = "10.0.2.0/24",
-  "us-east-1b" = "10.0.3.0/24"
-}
-EOT
-  default = {
-    "us-east-1a" = "10.0.2.0/24"
-    "us-east-1b" = "10.0.3.0/24"
-  }
+  type    = list(string)
+  default = ["10.0.3.0/24","10.0.4.0/24"]
 }
