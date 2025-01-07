3dac07d3aae35710aa0df325421a1816890889f5
carlosportella16 <carlosportella16@hotmail.com>
2025-01-07 18:40:15 -0300

    Fix: CIDs subnets

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -19,13 +19,13 @@ variable "az_count" {
 variable "public_subnet_cidrs" {
   type = list(string)
   description = "Os blocos CIDR para as sub-redes públicas"
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.3.0/24"] # alterado para 10.0.3.0/24
 }
 
 variable "private_subnet_cidrs" {
   type = list(string)
   description = "Os blocos CIDR para as sub-redes privadas"
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  default = ["10.0.101.0/24", "10.0.103.0/24"] # alterado para 10.0.103.0/24
 }
 
 variable "enable_dns_hostnames" {
